18d8403bbea44623ceca379a7fea3cd4cf27ceaa
Onyango Onyango <147000453+onyangoju@users.noreply.github.com>
2025-03-25 15:04:23 +0300

    Update wk3.py

--- wk3.py
+++ wk3.py
@@ -4,3 +4,18 @@ if temperature > 20:
     print("It's hotter today.")
 else:
     print("It's a cold day.")
+
+
+
+marks = 85  # You can change the marks value
+
+if marks > 90:
+    print("Grade A")
+elif marks > 80:
+    print("Grade B")
+elif marks > 70:
+    print("Grade C")
+elif marks > 60:
+    print("Grade D")
+else:
+    print("Grade F")
